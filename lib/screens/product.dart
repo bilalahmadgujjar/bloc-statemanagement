010bdf634a70c340953cfca963c221e61d81f7f8
MUHAMMAD BILAL AHMAD <bilaltech271@gmail.com>
2026-05-19 07:03:23 +0500

    bloc consumer done

--- lib/screens/product.dart
+++ lib/screens/product.dart
@@ -11,7 +11,6 @@ class ProductList extends StatefulWidget {
 }
 
 class _ProductListState extends State<ProductList> {
-
   @override
   void initState() {
     context.read<ProductsBloc>().add(ProductsEventLoaded());
@@ -23,61 +22,97 @@ class _ProductListState extends State<ProductList> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.blue,
-          centerTitle: true,
-          title: Text("Product Screen"),
-        ),
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        centerTitle: true,
+        title: Text("Product Screen"),
+      ),
 
-        /// =======================================
-        /// ITs is the bloc, Bloc Builder, Bloc Repo, Bloc Repo example
-        /// =======================================
+      /// =======================================
+      /// ITs is the bloc, Bloc Builder, Bloc Repo, Bloc Repo example
+      /// =======================================
 
-        // body: BlocBuilder<ProductsBloc, ProductsState>(
-        //   builder: (context, state) {
-        //     if (state is ProductsLoadingState) {
-        //       return Center(child: CircularProgressIndicator.adaptive());
-        //     } else if (state is ProductsLoadedState) {
-        //
-        //       return ListView.builder(
-        //         itemCount:  state.productsModel.length,
-        //         itemBuilder: (context, index) {
-        //           return ListTile(
-        //             leading:Image.network(state.productsModel[index].image),
-        //             title: Text(state.productsModel[index].title),
-        //             subtitle: Text(state.productsModel[index].description),
-        //           );
-        //         },
-        //       );
-        //     } else if (state is ProductsErrorState) {
-        //       return Center(child: Text(state.errorMessage));
-        //     } else {
-        //       return SizedBox();
-        //     }
-        //   },
-        // ),
-        //
+      // body: BlocBuilder<ProductsBloc, ProductsState>(
+      //   builder: (context, state) {
+      //     if (state is ProductsLoadingState) {
+      //       return Center(child: CircularProgressIndicator.adaptive());
+      //     } else if (state is ProductsLoadedState) {
+      //
+      //       return ListView.builder(
+      //         itemCount:  state.productsModel.length,
+      //         itemBuilder: (context, index) {
+      //           return ListTile(
+      //             leading:Image.network(state.productsModel[index].image),
+      //             title: Text(state.productsModel[index].title),
+      //             subtitle: Text(state.productsModel[index].description),
+      //           );
+      //         },
+      //       );
+      //     } else if (state is ProductsErrorState) {
+      //       return Center(child: Text(state.errorMessage));
+      //     } else {
+      //       return SizedBox();
+      //     }
+      //   },
+      // ),
+      //
 
-        /// =======================================
-        /// ITs is the bloc listener example (its use only for like listen activity in it
-        /// =======================================
+      /// =======================================
+      /// ITs is the bloc listener example (its use only for like listen activity in it
+      /// =======================================
 
-        body: BlocListener<ProductsBloc, ProductsState>(
-          listener: (context, state) {
-           if(state is ProductsLoadedState)
-             {
-               ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Data Loaded')));
+      // body: BlocListener<ProductsBloc, ProductsState>(
+      //   listener: (context, state) {
+      //    if(state is ProductsLoadedState)
+      //      {
+      //        ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Data Loaded')));
+      //
+      //      }
+      //    else if (state is ProductsErrorState)
+      //      {
+      //        ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Data not Loaded')));
+      //      }
+      //   },
+      //   child: Center(child: Text('Product Data'),),
+      // )
 
-             }
-           else if (state is ProductsErrorState)
-             {
-               ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Data not Loaded')));
-             }
-          },
-          child: Center(child: Text('Product Data'),),
-        )
+      /// =======================================
+      /// ITs is the bloc Consumer  (in this bloc builder and bloc listener both in it)
+      /// =======================================
+      body: BlocConsumer<ProductsBloc, ProductsState>(
+        listener: (context, state) {
+          if (state is ProductsLoadedState) {
+            ScaffoldMessenger.of(
+              context,
+            ).showSnackBar(SnackBar(content: Text('Data Loaded')));
+          } else if (state is ProductsErrorState) {
+            ScaffoldMessenger.of(
+              context,
+            ).showSnackBar(SnackBar(content: Text('Data not Loaded')));
+          }
+        },
 
-
+        builder: (context, state) {
+          if (state is ProductsLoadingState) {
+            return Center(child: CircularProgressIndicator.adaptive());
+          } else if (state is ProductsLoadedState) {
+            return ListView.builder(
+              itemCount: state.productsModel.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  leading: Image.network(state.productsModel[index].image),
+                  title: Text(state.productsModel[index].title),
+                  subtitle: Text(state.productsModel[index].description),
+                );
+              },
+            );
+          } else if (state is ProductsErrorState) {
+            return Center(child: Text(state.errorMessage));
+          } else {
+            return SizedBox();
+          }
+        },
+      ),
     );
   }
 }
